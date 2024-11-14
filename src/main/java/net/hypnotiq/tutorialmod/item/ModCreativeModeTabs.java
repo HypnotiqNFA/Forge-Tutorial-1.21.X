05d228097439d3293ac20b44b21cf88c223643dc
HypnotiqNFA <jashwanthpodugu09@gmail.com>
2024-11-14 17:22:33 -0500

    drops and recipes

--- src/main/java/net/hypnotiq/tutorialmod/item/ModCreativeModeTabs.java
+++ src/main/java/net/hypnotiq/tutorialmod/item/ModCreativeModeTabs.java
@@ -9,7 +9,6 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.RegistryObject;
-import org.lwjgl.system.linux.Stat;
 
 public class ModCreativeModeTabs {
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
