915a4df53d97797c9679a1170d825702e625be9e
HypnotiqNFA <jashwanthpodugu09@gmail.com>
2025-05-12 18:42:55 -0400

    Funny Sheep Update

--- src/main/java/net/hypnotiq/tutorialmod/event/ModEvents.java
+++ src/main/java/net/hypnotiq/tutorialmod/event/ModEvents.java
@@ -3,13 +3,27 @@ package net.hypnotiq.tutorialmod.event;
 
 import net.hypnotiq.tutorialmod.TutorialMod;
 import net.hypnotiq.tutorialmod.item.custom.HammerItem;
+import net.minecraft.client.renderer.EffectInstance;
 import net.minecraft.core.BlockPos;
+import net.minecraft.gametest.framework.GameTestHelper;
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.animal.Sheep;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.event.entity.living.LivingDamageEvent;
+import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.event.level.BlockEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import org.apache.commons.lang3.tuple.Triple;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -41,5 +55,33 @@ public class ModEvents {
                 HARVESTED_BLOCKS.remove(pos);
             }
         }
+    }
+
+    @SubscribeEvent
+    public static void onLivingDamage(LivingDamageEvent event) {
+        if(event.getEntity() instanceof Sheep sheep && event.getSource().getDirectEntity() instanceof Player player) {
+            player.sendSystemMessage(Component.literal(player.getDisplayName().getString()).append(" hit a sheep!"));
+            float health = sheep.getMaxHealth();
+            float damage = event.getAmount();
+            if(damage < health) {
+                if(damage/health > 0.5) {
+                    createSheep(sheep);
+                    createSheep(sheep);
+                } else {
+                    createSheep(sheep);
+                }
+                player.hurt(event.getSource(), damage);
+            }
+        }
+    }
+
+    public static void createSheep(Sheep sheep) {
+        ServerLevel serverLevel = (ServerLevel) sheep.level();
+        Sheep newsheep = EntityType.SHEEP.create(serverLevel);
+        EntityType.SHEEP.create(serverLevel);
+        if(newsheep != null) {
+            newsheep.moveTo(sheep.getX(), sheep.getY(), sheep.getZ(), 0.0F, 0.0F);
+            serverLevel.addFreshEntity(newsheep);
+            }
     }
 }
